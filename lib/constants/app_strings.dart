4025d035245d3dfc63bd9b53cd2608e5eac59152
Dapo <odapo138@gmail.com>
2025-02-18 06:04:02 +0100

    feat: update experience years in about me section

--- lib/constants/app_strings.dart
+++ lib/constants/app_strings.dart
@@ -1,6 +1,6 @@
 class AppString {
   static const aboutMe =
-      '''I'm Oladapo Daniel Olatubosun, a Flutter Mobile Developer with over 3 years of experience crafting iOS and Android applications. I've worked on impactful projects across industries, including MoniMoore, a financial app empowering users to manage their finances efficiently, and Kunda Kids, a gamified children's learning app.
+      '''I'm Oladapo Daniel Olatubosun, a Flutter Mobile Developer with over 4 years of experience crafting iOS and Android applications. I've worked on impactful projects across industries, including MoniMoore, a financial app empowering users to manage their finances efficiently, and Kunda Kids, a gamified children's learning app.
 
 Skilled in Flutter/Dart, Firebase, and state management architectures (BLoC, GetX, Provider), I deliver robust, scalable apps with clean architecture and exceptional user experiences. From GPS tracking and real-time video calls to secure payment integrations, I specialize in turning ideas into seamless, user-focused solutions.''';
 }
