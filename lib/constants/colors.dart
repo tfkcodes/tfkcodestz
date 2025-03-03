74669a1ef4ec9836a3dd8931116592370ff97c53
tfkcodes <lucianojackson048@gmail.com>
2025-03-03 10:05:20 +0300

    Update image assets and refactor color constants

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 
-const Color paleSlate = Color(0xFFccbdc5);
-const Color ebony = Color(0xFF0a0612);
-const Color studio = Color.fromARGB(255, 125, 79, 240);
-const Color valhalla = Color(0xFF311a5f);
-const Color donJuan = Color(0xFF634c51);
-const Color hippieBlue = Color(0xFF6998b6);
-const Color rouge = Color(0xFFa24379);
-const Color revolver = Color(0xFF271b3d);
-const Color cocoaBrown = Color(0xFF332222);
-const Color capeCod = Color.fromARGB(255, 93, 95, 95);
+const Color paleSlate = Color(0xFFC4A484);
+const Color ebony = Color(0xFF3E2723);
+const Color studio = Color(0xFFD9A066);
+const Color valhalla = Color(0xFF5D4037);
+const Color donJuan = Color(0xFF8B5A2B);
+const Color hippieBlue = Color(0xFFE0C097);
+const Color rouge = Color(0xFFBA7F42);
+const Color revolver = Color(0xFF4A2C2A);
+const Color cocoaBrown = Color(0xFF3E2723);
+const Color capeCod = Color(0xFFF5F5DC);
 const Color black = Color(0xFF021528);
 const Color white = Color(0xFFFFFFFF);
-
-// const Color purple = Color(0xFF623fbd);
